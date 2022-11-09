4a734c281265eb9573b2e58105442b8a5b0d7e7f
DarkPanth3r <marcospais@outlook.pt>
2022-11-09 16:48:25 +0100

    vowel update

--- Cisco_Academy/vowel_eater.py
+++ Cisco_Academy/vowel_eater.py
@@ -6,16 +6,14 @@ user_word = user_word.upper()
 for letter in user_word:
     # Complete the body of the for loop.
     if letter == "A":
-            if letter == "E":
-                continue
-    print(letter)
-    
-#    if letter == "E":
-#        continue
-#    print(letter)
-#    elif (letter == 'E'):
-#        continue
-#    print(letter)
-
-#    else (letter == "I"):
-#    print(letter)
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
