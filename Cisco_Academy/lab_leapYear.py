6342be86be2e99271e27dda1946b2b4dfa125e4c
darkpanth3r <marcospais@outlook.pt>
2023-01-05 18:34:36 +0000

    lab in progress

---
+++ Cisco_Academy/lab_leapYear.py
@@ -0,0 +1,17 @@
+# 4.3.1.6 LAB: A leap year: writing your own functions 
+
+def is_year_leap(year):
+#
+# put your code here
+#
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+	yr = test_data[i]
+	print(yr,"->",end="")
+	result = is_year_leap(yr)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
