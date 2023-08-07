b1ee2efe189b538344a5eb8b93db52c187f828dd
mpais <marcospais@outlook.pt>
2023-08-07 17:18:42 +0100

    lab 2.4

---
+++ Cisco_Academy/lab_2_4_10.py
@@ -0,0 +1,14 @@
+# 2.4.10   LAB   Operators and expressions
+
+x = 1
+
+x = float(x) 
+
+# write your code here 
+a = 3 * pow(x,3) 
+b = 2 * pow(x,2) 
+c = 3 * x - 1 
+
+y = a - b + c  
+
+print("y =", y) 
