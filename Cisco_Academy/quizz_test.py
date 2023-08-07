da3c396097ae7a0d6252cce818b7e70b6c920b54
mpais <marcospais@outlook.pt>
2023-08-07 17:14:52 +0100

    lab 2.4

---
+++ Cisco_Academy/quizz_test.py
@@ -0,0 +1,11 @@
+print("My\nname\nis\nBond.", end=" ")
+print("James Bond.")
+
+# print(sep="&", "fish", "chips")
+# SyntaxError: positional argument follows keyword argument
+
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+#print('"Greg's book."')
