8be2285070a39d551bbf087b034a9b480597bcfb
mpais <marcospais@outlook.pt>
2023-08-08 13:54:08 +0100

    lab 3.1

--- Cisco_Academy/calendar.py
+++ Cisco_Academy/calendar.py
@@ -1,20 +1,16 @@
+# 3.1.12 LAB Essentials of the if-elif-else statement
+
 year = int(input("Enter a year: "))
 
-#
-# Write your code here.
-#	
-
 if year < 1582:
-    print("Not within the Gregorian calendar period")
-
-elif (year % 4) != 0:
-    print("Common year")
-
-elif (year % 100) != 0:
-    print("Leap year")
-
-elif (year % 400) != 0:
-    print("Common year")
-
+	print("Not within the Gregorian calendar period")
 else:
-    print("Leap year")
+    #  Write the if-elif-elif-else block here.
+    if (year % 4) != 0:
+        print("Common year")
+    elif (year % 100) != 0:
+        print("Leap year")
+    elif (year % 400) != 0:
+        print("Common year")
+    else:
+        print("Leap year")
